142c6d51a032693c3b8bbc13036a7554fd87636a
Ricardo Alves <dev.rical21@gmail.com>
2021-07-20 20:30:37 -0300

    Java Exceptions (pratica) 20/07/2021

--- Exceptions/src/com/application/Main.java
+++ Exceptions/src/com/application/Main.java
@@ -1,5 +1,7 @@
 package com.application;
 
+import model.entities.Account;
+import model.exceptions.DomainException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,16 +10,28 @@ public class Main {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
-        try {
+        try (Scanner sc = new Scanner(System.in)) {  // o objeto Scanner pode ser instanciado no bloco try
 
-        }
-        catch () {
+            System.out.println();
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("Holder: ");
+            sc.nextLine();  // consumir quebra de linha
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            Double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            Double withdrawLimit = sc.nextDouble();
+            System.out.print("Enter amount for withdraw: ");
+            Double amount = sc.nextDouble();
+            Account c1 = new Account(number, holder, balance, withdrawLimit);
+            c1.withdraw(amount);
+            System.out.println(c1.newBalance());
 
-        }
-        finally {
-            sc.close();
+        } catch (DomainException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
         }
 
     }
--- Exceptions/src/model/exceptions/Exceptions.java
+++
@@ -1,5 +0,0 @@
-package model.exceptions;
-
-public class Exceptions {
-
-}
