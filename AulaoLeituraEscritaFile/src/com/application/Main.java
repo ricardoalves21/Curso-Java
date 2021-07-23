22d001107a00f5371feeda0e8b0a1917bb3a5772
Ricardo Alves <dev.rical21@gmail.com>
2021-07-23 11:22:44 -0300

    Aulão leitura e escrita com file e scanner 23/07/2021

--- AulaoLeituraEscritaFile/src/com/application/Main.java
+++ AulaoLeituraEscritaFile/src/com/application/Main.java
@@ -8,15 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File("c:\\temp\\file.csv");  // instanciando um objeto do tipo FILE que está apontando para este endereço
-        Scanner sc = null;
+        File file = new File("c:\\temp\\file.csv");  // instanciando o objeto 'file' do tipo FILE que está apontando para o endereço de um arquivo
+        Scanner sc = null;  // criando a variável 'sc' do tipo Scanner recebendo null
 
         try {
 
-            sc = new Scanner(file);
+            sc = new Scanner(file);  // fazendo a variável 'sc' ser um objeto Scanner para ler o arquivo referenciado no objeto 'file'
             while (sc.hasNextLine()) {  // enquanto não chegar no fim do arquivo continue lendo linha a linha
-                String line = sc.nextLine();
-                System.out.println(line);
+                String line = sc.nextLine();  // a variável 'line' gravará cada linha que está sendo lida pelo objeto 'sc'
+                System.out.println(line);  // imprima cada uma das linhas que foi gravada no objeto 'line'
             }
 
         } catch (IOException e) {
@@ -25,7 +25,7 @@ public class Main {
 
         } finally {
 
-            if (sc != null) {
+            if (sc != null) {  // o objeto 'sc' só poderá ser fechado caso tenha sido iniciado, ou seja, caso não seja null
                 sc.close();
             }
         }
