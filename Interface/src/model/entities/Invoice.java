7e6a2fb37d6c72a38090e0e26367ca7e058d5a8f
Ricardo Alves <dev.rical21@gmail.com>
2021-07-26 22:32:37 -0300

    Aula teórica de Interface 26/07/2021

---
+++ Interface/src/model/entities/Invoice.java
@@ -0,0 +1,35 @@
+package model.entities;
+
+public class Invoice {  // fatura
+
+    private Double basicPayment; // pagamento básico
+    private Double tax;  // imposto
+
+    public Invoice() {
+    }
+
+    public Invoice(Double basicPayment, Double tax) {
+        this.basicPayment = basicPayment;
+        this.tax = tax;
+    }
+
+    public Double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public void setBasicPayment(Double basicPayment) {
+        this.basicPayment = basicPayment;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+
+    public Double getTotalPayment() {
+        return getBasicPayment() + getTax();
+    }
+}
---
+++ Interface/src/model/service/RentalService.java
@@ -0,0 +1,36 @@
+package model.service;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {  // serviço de aluguel
+
+    private Double pricePerDay;  // preço por dia
+    private Double pricePerHour;  // preço por hora
+
+    private BrazilTaxService taxService;  // imposto pelo serviço
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {  // processar fatura
+        long t1 = carRental.getStart().getTime(); // esse comando pega o valor em milisegundos da minha data inicial
+        long t2 = carRental.getFinish().getTime(); // esse comando pega o valor em milisegundas da minha data final
+        double hours = (double) (t2 - t1) / 1000 / 60 / 60;  // pegamos o resultado em double para termos o valor em decimais a fim de que possamos fazer a operação matemática de arredondamento
+        double basicPayment;  // pagamento básico
+
+        if (hours <= 12.0) {
+            basicPayment = Math.ceil(hours) * pricePerDay;
+        } else {
+            basicPayment = Math.ceil(hours / 24) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);  // imposto
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+
+    }
+}
