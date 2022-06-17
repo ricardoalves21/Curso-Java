2b70245c7fab11efd675fa941848da282436da85
Ricardo Alves <dev.rical21@gmail.com>
2022-06-17 08:57:17 -0300

    Estudo banco de dados com Java

--- jdbc2/src/application/Program.java
+++ jdbc2/src/application/Program.java
@@ -9,17 +9,35 @@ import java.text.SimpleDateFormat;
 public class Program {
 
 
+
+    // Método Principal
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");  // criei um padrão para datas
+
+
+        // Criando variáveis e objetos a serem utilizados neste código
         Connection conn = null;  // criei uma variável para guardar a conexão com o banco de dados
         PreparedStatement st = null;  // criei uma variável para escrever minhas queryes com o banco de dados
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");  // criei uma variável para estabelecer um padrão para datas
+
 
         try {
 
             conn = DB.getConnection();  // a variável 'conn' está chamando o método 'getConnection()' da classe DB
 
+
+
             /*
+            st = conn.prepareStatement(
+                    "DELETE FROM seller "
+                            + "WHERE id = ? ",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setInt(1, 7);
+            */
+
+
+
             st = conn.prepareStatement(
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
@@ -29,39 +47,46 @@ public class Program {
 
             st.setString(1, "Ricardo");
             st.setString(2, "ricardo@email.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 3000.0);
+            st.setDate(3, new java.sql.Date(sdf.parse("21/08/1981").getTime()));
+            st.setDouble(4, 10000.0);
             st.setInt(5, 4);
-            */
+
+
+
 
             /*
             st = conn.prepareStatement("INSERT INTO department (Name) VALUES ('D1'), ('D2')",
                     Statement.RETURN_GENERATED_KEYS);  // executou a query retornou os valores das chaves geradas
+             */
 
+
+
+            // Bloco de código para registrar as linhas afetadas nas tabelas
             int rowsAffected = st.executeUpdate();  // a variável 'rowsAffected' está guardando a quantidade de registros gerados pela query
 
-            if (rowsAffected > 0) {  // se a quantidade de registros gerados pela query for maior que zero
-                ResultSet rs = st.getGeneratedKeys();  // a variável rs do tipo ResultSet guardará todas as chaves geradas pela query
-                while (rs.next()) {  // enquanto houver registros para ler
+            if (rowsAffected > 0) {  // se a quantidade de registros gerados pela query e guardados na variável 'rowsAffected' for maior que zero
+                ResultSet rs = st.getGeneratedKeys();  // a variável 'rs' do tipo 'ResultSet' guardará todas as chaves geradas pela query
+
+                while (rs.next()) {  // enquanto houver registros para ler na variável 'rs' do tipo 'ResultSet'
                     int id = rs.getInt(1);  // a variável 'id' receberá o valor da primeira coluna do resultSet
                     System.out.println("Done! Id = " + id);  // apresentar a mensagem informando o valor da variável 'id'
                 }
+
             }
             else {
                 System.out.println("No rows affected!");
             }
-             */
-
-            st = conn.prepareStatement("CREATE TABLE Cliente (Codigo INT NOT NULL AUTO_INCREMENT, Nome VARCHAR (60) NOT NULL, Data_Nascimento DATE, Telefone CHAR (8), PRIMARY KEY (Codigo))");
 
         }
         catch (SQLException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatment(st);  // antes de fechar a conexão é preciso fechar a declaração do sql
+            DB.closeConnection();  // fechar a conexão sempre por último
         }
-        finally {
-            DB.closeStatment(st);
-            DB.closeConnection();
-        }
+
 
 
     }
--- jdbc2/src/db/DB.java
+++ jdbc2/src/db/DB.java
@@ -9,74 +9,87 @@ public class DB {
 
 
 
+    // ATRIBUTOS
     private static Connection conn = null;
 
 
 
+    // Método para LER dados de entrada
     private static Properties loadProperties() {
 
-        try (FileInputStream fs = new FileInputStream("db.properties")) {
-            Properties props = new Properties();
-            props.load(fs);
-            return props;
+        try (FileInputStream fs = new FileInputStream("db.properties")) {  // apontando a leitura para o arquivo 'db.properties' para dentro do objeto 'fs'
+            Properties props = new Properties();  // Criando um objeto do tipo 'Properties'
+            props.load(fs);  // Fazendo o objeto 'props' rodar o arquivo guardado dentro do objeto 'fs'
+            return props;  // Retornando o objeto 'props' que rodou o arquivo 'db.properties' guardado no objeto 'fs'
         }
-        catch (IOException e) {
-            throw new DbException(e.getMessage());
+        catch (IOException e) {  // Caso seja gerada uma exceção
+            throw new DbException(e.getMessage());  // A classe 'DbException' chamará o seu construtor passando a mensagem capturada pela classe 'IOException' 'e.getMessage()'
         }
+
     }
 
 
 
+    // Método para pegar a conexão
     public static Connection getConnection() {
 
-        if (conn == null) {
+        if (conn == null) {  // Caso a conexão não tenha sido iniciada
             try {
-                Properties props = loadProperties();
-                String url = props.getProperty("dburl");
-                conn = DriverManager.getConnection(url, props);
+                Properties props = loadProperties();  // A variável 'props' receberá o método 'loadProperties' criado anteriormente
+                String url = props.getProperty("dburl");  // A variável 'url' pegará a propriedade 'dburl' de dentro da variável 'props'
+                conn = DriverManager.getConnection(url, props);  // A variável 'conn' receberá da classe 'DriverManager' a conexão que foi pega da variável 'props' e da propriedade capturada pela variável 'url'
             }
             catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
         }
-        return conn;
+        return conn;  // Retornará a variável 'conn' responsável por estabelecer a conexão
     }
 
 
 
+    // Método para fechar a conexão
     public static void closeConnection() {
-        if (conn != null) {
+
+        if (conn != null) {  // Se a conexão estiver estabelecida
             try {
-                conn.close();
+                conn.close();  // Fechar a conexão
             }
             catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
         }
+
     }
 
 
 
+    // Método para fechar as declarações de sql (queryes)
     public static void closeStatment(Statement st) {
-            if (st != null) {
+
+            if (st != null) {  // Se a variável 'st' estiver em uso
                 try {
-                    st.close();
+                    st.close();  // Encerrar a variável 'st'
                 } catch (SQLException e) {
                     throw new DbException(e.getMessage());
                 }
             }
+
     }
 
 
 
+    // Método para fechar o 'ResultSet'
     public static void closeResultSet(ResultSet rs) {
-        if (rs != null) {
+
+        if (rs != null) {  // Se o resultSet estiver em uso
             try {
-                rs.close();
+                rs.close();  // Fechar o resultSet
             } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
         }
+
     }
 
 
