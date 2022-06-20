ecdaef64147265532aa39cebb6083b69ec05de38
Ricardo Alves <dev.rical21@gmail.com>
2022-06-20 14:54:46 -0300

    Criando a class Seller

---
+++ demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
+
+public class Program {
+
+
+
+    // Método Principal
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
+
+    }
+}
