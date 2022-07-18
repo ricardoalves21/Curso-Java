e4855feef124e0ffdc8064519e520c08cf79d27b
ricardo.alves <ricardo.alves@fourbank.com.br>
2022-07-18 08:57:01 -0300

    Pratica POO Java

---
+++ PraticaPoo1/src/entities/Categoria.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Categoria {
+
+
+    // ATRIBUTOS
+    public String nmCategoria;
+
+
+
+    // MÉTODOS ACESSORES
+    public String getNmCategoria() {
+        return nmCategoria;
+    }
+
+    public void setNmCategoria(String nmCategoria) {
+        this.nmCategoria = nmCategoria;
+    }
+
+}
---
+++ PraticaPoo1/src/entities/Departamento.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Departamento {
+
+
+    // ATRIBUTOS
+    public String nmDepartamento;
+
+
+
+    // MÉTODOS ACESSORES
+    public String getNmDepartamento() {
+        return nmDepartamento;
+    }
+
+    public void setNmDepartamento(String nmDepartamento) {
+        this.nmDepartamento = nmDepartamento;
+    }
+
+}
